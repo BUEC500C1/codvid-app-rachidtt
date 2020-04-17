cc8229fb5b0594deaccc66f24b30ba58daf31b38
rachidtt <rachidtt@bu.edu>
2020-04-17 17:47:00 -0400

    Added map

--- covid/App.js
+++ covid/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import MapView from 'react-native-maps'; //new
+import {Dimensions} from 'react-native';//new
+/*
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>HELLO WORLD COVIDAPP</Text>
     </View>
   );
+}*/
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView style={styles.mapStyle} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -15,5 +27,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
